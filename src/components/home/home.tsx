b9e4df371ae3180e7192d981eaca3caa133a4dea
birhanu-dejen <birhanudejen617@gmail.com>
2025-01-03 00:56:05 +0300

    make home structure

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,10 +1,21 @@
 import styles from "./home.module.css";
-import background_image from "../../assets/background.png";
+import background_image from "../../assets/home-background.webp";
 function Home() {
   return (
-    <div className={styles.container}>
+    <section className={styles.container}>
+      <h2>
+        Hi 👋, It's <span>Birhanu Dejen</span>
+        <div>
+          I'm a Full-stack developer based in Addis Ababa, Ethiopia (UTC+3) I
+          have experience in developing web applications using modern
+          technologies. Proficient in both front-end and back-end development,
+          with a strong understanding of data-structure and algorithm ,
+          Passionate about creating clean, efficient, and scalable code that
+          delivers a great user experience.
+        </div>
+      </h2>
       <img className={styles.image} src={background_image} alt="background" />
-    </div>
+    </section>
   );
 }
 export default Home;
